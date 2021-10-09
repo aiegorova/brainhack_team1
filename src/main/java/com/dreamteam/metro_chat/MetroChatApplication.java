569413195eb13858c3a5c53d61721cf841629476
aiegorova <ermilovaalena@gmail.com>
2021-10-09 11:17:04 +0300

    initial commit

---
+++ src/main/java/com/dreamteam/metro_chat/MetroChatApplication.java
@@ -0,0 +1,13 @@
+package com.dreamteam.metro_chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetroChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetroChatApplication.class, args);
+	}
+
+}
