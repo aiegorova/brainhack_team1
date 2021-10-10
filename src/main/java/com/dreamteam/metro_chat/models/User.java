21161267423b3baebbc32557d766d8c61ea3eaba
SokolovAl <sokolovmono@gmail.com>
2021-10-10 20:10:30 +0300

    find user email method

--- src/main/java/com/dreamteam/metro_chat/models/User.java
+++ src/main/java/com/dreamteam/metro_chat/models/User.java
@@ -8,16 +8,26 @@ import java.util.Objects;
 public class User {
 
     @Id
-    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column
     private String name;
 
-    public User() {}
 
-    public User(String name) {
+    @Column(name = "email", unique = true)
+    private String email;
+
+    public User() {
+    }
+
+    public User(String name, String email) {
         this.name = name;
+        this.email = email;
+    }
+
+    public boolean isEmail(String email) {
+        return email.matches("^[-0-9a-zA-Z.+_]+@[-0-9a-zA-Z.+_]+\\.[a-zA-Z]{2,4}");
     }
 
     public Long getId() {
@@ -36,21 +46,29 @@ public class User {
         this.name = name;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         User user = (User) o;
-        return Objects.equals(id, user.id) && Objects.equals(name, user.name);
+        return Objects.equals(id, user.id) && Objects.equals(name, user.name) && Objects.equals(email, user.email);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, name);
+        return Objects.hash(id, name, email);
     }
 
     @Override
     public String toString() {
-        return "User{" + "id=" + id + ", name='" + name + '\'' + '}';
+        return "User{" + "id=" + id + ", name='" + name + '\'' + ", email='" + email + '\'' + '}';
     }
 }
